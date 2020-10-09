33311962951125488937ab7229681a706134e7ee
Filipe Kramer <filipekramer.dev@gmail.com>
2020-10-09 12:21:30 -0300

    Header e sobre finish desk e mobile

---
+++ js/text-animate.js
@@ -0,0 +1,41 @@
+// Animate Text
+var div = document.getElementById('animate-text');
+var textos = ['Front-end Developer', 'Web Design' , 'UX/UI'];
+
+function escrever(str, done) {
+    var char = str.split('').reverse();
+    var typer = setInterval(function() {
+        if (!char.length) {
+            clearInterval(typer);
+            return setTimeout(done, 600); // só para esperar um bocadinho
+        }
+        var next = char.pop();
+        div.innerHTML += next;
+    }, 130);
+}
+
+function limpar(done) {
+    var char = div.innerHTML;
+    var nr = char.length;
+    var typer = setInterval(function() {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return done();
+        }
+        div.innerHTML = char.slice(0, nr);
+    }, 85);
+}
+
+function rodape(conteudos, el) {
+    var atual = -1;
+	function prox(cb){
+		if (atual < conteudos.length - 1) atual++;
+		else atual = 0;
+		var str = conteudos[atual];
+		escrever(str, function(){
+			limpar(prox);
+		});
+	}
+	prox(prox);
+}
+rodape(textos);
